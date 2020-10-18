4ae7aecd10115c0d2a1197fe5a282d98510b16c8
Clifford D. R. Mantow <s21810434@student.unklab.ac.id>
2020-10-19 01:49:01 +0800

    finished function groupbox selection

--- main.js
+++ main.js
@@ -15,6 +15,128 @@ const studnents = [
     }
 ]
 
+
+const daftarFakultas = [
+	{
+        fakultas:"Fakultas Ilmu Komputer",
+        jurusan: [
+            'Informatika', 
+            'Sistem Informasi'
+        ]
+	},
+	{
+		fakultas:"Pascasarjana",
+        jurusan: [
+            'Magister Manajemen',
+            'Magister Teologi'
+        ]
+	},
+	{
+		fakultas:"Fakultas Filsafat",
+		jurusan: [
+            'Ilmu Filsafat'
+        ]
+	},
+	{
+		fakultas:"Fakultas Keguruan dan Ilmu Pendidikan",
+		jurusan: [
+			'Pendidikan Agama',
+			'Pendidikan Bahasa Inggris',
+			'Pendidikan Ekonomi',
+            'Pendidikan Luar Sekolah'
+        ]
+	},
+	{
+		fakultas:"Fakultas Ekonomi dan Bisnis",
+		jurusan: [
+            'Akuntansi', 
+            'Manejemen'
+        ]
+	},
+	{
+		fakultas:"Fakultas Pertanian",
+		jurusan: [
+            'Agroteknologi'
+        ]
+	},
+	{
+		fakultas:"Fakultas Keperawatan",
+		jurusan: [
+            'Profesi Ners', 
+            'Keperawatan'
+        ]
+	},
+]
+
+// show-hide button setup
+const form = document.querySelector('#show-hide');
+const buttonShowHide = document.querySelector('#button-show-hide');
+
+buttonShowHide.addEventListener("click", () => {
+    if(form.style.display === "none")
+    {
+        form.style.display = "block";
+        buttonShowHide.textContent = "Hide Form Add New Student";
+    }
+    else{
+        form.style.display = "none";
+        buttonShowHide.textContent = "Show Form Add New Student";
+    }
+});
+
+     //funciton groupbox selection
+
+//fakultas
+const listFaculty = document.querySelector('#student-faculty');
+const tagFaculty = document.createElement('option');
+
+for(dF of daftarFakultas){
+	let tagFakultas = document.createElement('option');
+	let textFakultas = document.createTextNode(dF.fakultas);
+	tagFakultas.appendChild(textFakultas);
+	listFaculty.appendChild(tagFakultas);
+}
+
+//prodi
+const listProdi = document.querySelector('#student-program-study');
+const tagProdi = document.createElement('option');
+
+listFaculty.addEventListener("click", (e) =>{
+    const selected = e.target.value;
+    let select;
+
+    listProdi.innerHTML = '<select class="form-control input-size" style="font-size: 13px;" id="student-faculty"><option>-- SELECT PROGRAM OF STUDY --</option></select>';
+
+    if(selected == "Fakultas Ilmu Komputer")                    { select = 0; }
+    else if(selected == "Pascasarjana")                         { select = 1; }
+    else if(selected == "Fakultas Filsafat")                    { select = 2; }
+    else if(selected == "Fakultas Keguruan dan Ilmu Pendidikan"){ select = 3; }
+    else if(selected == "Fakultas Ekonomi dan Bisnis")          { select = 4; }
+    else if(selected == "Fakultas Pertanian")                   { select = 5; }
+    else if(selected == "Fakultas Keperawatan")                 { select = 6; }
+
+    for(s of daftarFakultas[select].jurusan)
+    {
+        let tagProdi = document.createElement('option');
+        let textProdi = document.createTextNode(s);
+        tagProdi.appendChild(textProdi);
+        listProdi.appendChild(tagProdi);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // studnents.forEach((st) =>{
 //     console.log(st);
 // })
