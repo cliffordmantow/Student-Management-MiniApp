8421605e34b45a3c05a074e9181a11b8b6938da6
Clifford D. R. Mantow <s21810434@student.unklab.ac.id>
2020-10-18 17:47:11 +0800

    fixed html dan css

---
+++ main.js
@@ -0,0 +1,46 @@
+const studnents = [
+    {
+        NIM: 105021810058,
+        fullName: "Clifford Mantow",
+        gender: "Male",
+        faculty: "Fakultas Ilmu Komputer",
+        prodi: "Informatika"
+    },
+    {
+        NIM: 105021910056,
+        fullName: "Virginia Muaja",
+        gender: "Female",
+        faculty: "Unklab Business School",
+        prodi: "Accounting"
+    }
+]
+
+// studnents.forEach((st) =>{
+//     console.log(st);
+// })
+
+// const femaleStudent = studnents.filter((st) => {
+//     if(st.gender === "Female")
+//     {
+//         return true;
+//     }
+// });
+// console.log(femaleStudent[0].fullName);
+
+// const currentYear = new Date();
+// console.log(currentYear.getDate);
+
+//immediately invoked
+// const test = ((st) => st*st)(5);
+// console.log(test);
+
+    // function Person(firstName, age)
+    // {
+    //     this.firstName = firstName;
+    //     this.age = age;
+    // }
+
+    // const instPerson = new Person("Johhny", 21);
+    // console.log(instPerson); 
+
+
