3eddfe7edbe1e6fdde800924b54b1a0610b08991
Clifford D. R. Mantow <s21810434@student.unklab.ac.id>
2020-10-19 21:28:56 +0800

    Creating some function but not fixed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const studnents = [
+const students = [
     {
         NIM: 105021810058,
         fullName: "Clifford Mantow",
@@ -9,12 +9,11 @@ const studnents = [
     {
         NIM: 105021910056,
         fullName: "Virginia Muaja",
-        gender: "Female",
-        faculty: "Unklab Business School",
-        prodi: "Accounting"
+        genders: "Female",
+        faculty: "Fakultas Ekonomi dan Bisnis",
+        prodi: "Akuntansi"
     }
 ]
-
 
 const daftarFakultas = [
 	{
@@ -65,8 +64,11 @@ const daftarFakultas = [
             'Profesi Ners', 
             'Keperawatan'
         ]
-	},
+    }
 ]
+
+// insialisasi data awal
+updateList();
 
 // show-hide button setup
 const form = document.querySelector('#show-hide');
@@ -85,7 +87,6 @@ buttonShowHide.addEventListener("click", () => {
 });
 
      //funciton groupbox selection
-
 //fakultas
 const listFaculty = document.querySelector('#student-faculty');
 const tagFaculty = document.createElement('option');
@@ -96,7 +97,6 @@ for(dF of daftarFakultas){
 	tagFakultas.appendChild(textFakultas);
 	listFaculty.appendChild(tagFakultas);
 }
-
 //prodi
 const listProdi = document.querySelector('#student-program-study');
 const tagProdi = document.createElement('option');
@@ -107,7 +107,7 @@ listFaculty.addEventListener("click", (e) =>{
 
     listProdi.innerHTML = '<select class="form-control input-size" style="font-size: 13px;" id="student-faculty"><option>-- SELECT PROGRAM OF STUDY --</option></select>';
 
-    if(selected == "Fakultas Ilmu Komputer")                    { select = 0; }
+    if     (selected == "Fakultas Ilmu Komputer")               { select = 0; }
     else if(selected == "Pascasarjana")                         { select = 1; }
     else if(selected == "Fakultas Filsafat")                    { select = 2; }
     else if(selected == "Fakultas Keguruan dan Ilmu Pendidikan"){ select = 3; }
@@ -118,51 +118,220 @@ listFaculty.addEventListener("click", (e) =>{
     for(s of daftarFakultas[select].jurusan)
     {
         let tagProdi = document.createElement('option');
-        let textProdi = document.createTextNode(s);
+        let textProdi = document.createTextNode(s);    
+
         tagProdi.appendChild(textProdi);
         listProdi.appendChild(tagProdi);
     }
 });
 
+    //function to add new student
+    //button add student
+const buttonAdd = document.querySelector("#button-add-student");
 
+buttonAdd.addEventListener("click", () =>{
 
+    const newNIM = document.querySelector("#student-ID").value;
+    const newFullName = document.querySelector("#student-full-name").value;
+    const newGender = document.querySelector("input[name='custom-radio']:checked").value;
+    const newFaculty = document.querySelector("#student-faculty").options[document.querySelector("#student-faculty").selectedIndex].value;
+    const newProgramStudy = document.querySelector("#student-program-study").options[document.querySelector("#student-program-study").selectedIndex].value;
 
+    const newAdded = document.querySelector('#new-added');
+    newAdded.innerHTML = "";
 
+    // input data student ke list
+    if(newNIM !== "" && newFullName !== "" && newFaculty !== "-- SELECT FACULTY --" && newProgramStudy !=="-- SELECT PROGRAM OF STUDY --")
+    {
+        let isNumber = document.formInput.formNumber.value;
+        if(isNaN(isNumber))
+        {
+            alert("NIM tidak boleh di isi huruf");
+            updateList();
+        }
+        else
+        {
+            students.push({
+                NIM : newNIM,
+                fullName : newFullName,
+                genders : newGender,
+                faculty : newFaculty,
+                prodi : newProgramStudy
+            });
+            
+            updateList();
+            document.formInput.reset();
+            alert(`${newFullName} was added.`);
+        }
+    }
+    else
+    {
+        alert("Data belum lengkap");
+        updateList();
+    }
+});
 
+    // function list
+function updateList()
+{
+    const newAdded = document.querySelector('#new-added');
 
+    for(student of students)
+    {
+        const trTag = document.createElement("tr");
+        for(value in student)
+        {
+            const tdTag = document.createElement("td");
+            const text = document.createTextNode(student[value]);
+            tdTag.appendChild(text);
 
+            trTag.appendChild(tdTag);
+            newAdded.appendChild(trTag);
+        }
+        // delete button
+        const tdTag = document.createElement("td");
+        const icon = `<button type="button" class="btn btn-danger pl-2 pr-2 pt-1 pb-1"><i class="fas fa-user-minus"></i></button>`;
+        tdTag.innerHTML = icon;
+        trTag.appendChild(tdTag);
+    
+    }
+}
 
+    // search student by name
+const searchBox = document.querySelector("#search-student-name");
 
+searchBox.addEventListener("input",() => {
+    if(searchBox.length == 0)
+    {
+        updateList();
+    }
+    else
+    {
+        newAdded = document.querySelector("#new-added");
+        newAdded.innerHTML = "";
 
+        //filter the student
+        let searched_student = students.filter((s) => {
+            return s.fullName.toLowerCase().includes(searchBox.value.toLowerCase());
+        });
 
+        for(student of searched_student){
 
+            let tr = document.createElement("tr");
 
-// studnents.forEach((st) =>{
-//     console.log(st);
-// })
+            for(value in student){
 
-// const femaleStudent = studnents.filter((st) => {
-//     if(st.gender === "Female")
-//     {
-//         return true;
-//     }
-// });
-// console.log(femaleStudent[0].fullName);
+                let td = document.createElement("td");
+                td.appendChild(document.createTextNode(student[value]));
 
-// const currentYear = new Date();
-// console.log(currentYear.getDate);
+                tr.appendChild(td);
+            }
+            newAdded.appendChild(tr);
+        }
+    }
+});
 
-//immediately invoked
-// const test = ((st) => st*st)(5);
-// console.log(test);
+    // filter faculty
+const listFacultyFilter = document.querySelector('#filter-faculty');
 
-    // function Person(firstName, age)
-    // {
-    //     this.firstName = firstName;
-    //     this.age = age;
-    // }
+for(dfFilter of daftarFakultas){
+	let tagFakultasFilter = document.createElement('option');
+	let textFakultasFilter = document.createTextNode(dfFilter.fakultas);
+	tagFakultasFilter.appendChild(textFakultasFilter);
+	listFacultyFilter.appendChild(tagFakultasFilter);
+}
 
-    // const instPerson = new Person("Johhny", 21);
-    // console.log(instPerson); 
+const buttonFilter = document.querySelector("#button-filter-faculty");
 
+buttonFilter.addEventListener("click",() => {
+    const selected_faculty = listFacultyFilter.options[listFacultyFilter.selectedIndex].value;
 
+    if(selected_faculty == "-- SELECT FACULTY --")
+    {
+        updateList();
+    }
+    else
+    {
+        newAdded = document.querySelector("#new-added");
+        newAdded.innerHTML = "";
+
+        //filter the student
+        let searched_student = students.filter((s) => {
+            return s.faculty ==  selected_faculty;
+        });
+
+        for(student of searched_student){
+            const tdTag = document.createElement("td");
+            let tr = document.createElement("tr");
+
+            for(value in student){
+
+                let td = document.createElement("td");
+                td.appendChild(document.createTextNode(student[value]));
+
+                tr.appendChild(td);
+                newAdded.appendChild(tr);
+            }
+            // delete button
+            const icon = `<button type="button" class="btn btn-danger pl-2 pr-2 pt-1 pb-1"><i class="fas fa-user-minus"></i></button>`;
+            tdTag.innerHTML = icon;
+            tr.appendChild(tdTag);
+        }
+        // delete button
+        
+    }
+});
+
+    // fukter prodi
+const filter_by_program_study = document.querySelector("#filter-program-study");
+
+for(i of daftarFakultas){
+
+	for(j of i.jurusan){
+		const parent = document.createElement("option");
+		const child = document.createTextNode(j);
+		parent.append(child);
+		filter_by_program_study.appendChild(parent);
+	}
+}
+
+const buttonFilterProdi = document.querySelector("#button-filter-prodi");
+
+buttonFilterProdi.addEventListener("click",() => {
+    const selected_prodi = filter_by_program_study.options[filter_by_program_study.selectedIndex].value;
+
+    if(selected_prodi == "-- SELECT PROGRAM OF STUDY --")
+    {
+        updateList();
+    }
+    else
+    {
+        newAdded = document.querySelector("#new-added");
+        newAdded.innerHTML = "";
+
+        //filter the student
+        let searched_student = students.filter((s) => {
+            return s.prodi ==  selected_prodi;
+        });
+
+        for(student of searched_student){
+            const tdTag = document.createElement("td");
+            let tr = document.createElement("tr");
+
+            for(value in student){
+
+                let td = document.createElement("td");
+                td.appendChild(document.createTextNode(student[value]));
+
+                tr.appendChild(td);
+                newAdded.appendChild(tr);
+            }
+            // delete button
+            const icon = `<button type="button" class="btn btn-danger pl-2 pr-2 pt-1 pb-1"><i class="fas fa-user-minus"></i></button>`;
+            tdTag.innerHTML = icon;
+            tr.appendChild(tdTag);
+        }
+        // delete button
+        
+    }
+});
